3c523edae04e72ae27be54213e33f31069c7e4bd
Prith <prith98@gmail.com>
2021-12-27 11:19:55 -0800

    Created qna folder and file, dummy function just so we can import

---
+++ client/src/qna/qna.js
@@ -0,0 +1,13 @@
+import React, { useState, useContext, useEffect} from 'react';
+import {MainContext} from '../../contexts/contexts.js'
+import Axios from 'axios';
+
+function Qna () {
+
+  return (
+    <div>Hello World!</div>
+  )
+
+}
+
+export default Qna;
