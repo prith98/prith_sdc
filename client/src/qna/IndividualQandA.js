e0a19912456470202a9bedbe454652d131a978ef
Prith <prith98@gmail.com>
2021-12-28 10:47:34 -0800

    Re-formatted Question section to be up to par with business doc

--- client/src/qna/IndividualQandA.js
+++ client/src/qna/IndividualQandA.js
@@ -29,9 +29,12 @@ function IndividualQandA () {
       {/* Dynamically renders questions from currentQuestion prop in the format of Question, then Answer, then asker name, date asked, helpful, how many people found it helpful, and report*/}
       {currentQuestion.map(oneQuestion => (
         <div key={oneQuestion.question_id}>
-          <div>Q: {oneQuestion.question_body}</div>
+          <div>
+            Q: {oneQuestion.question_body}
+            <span> by {oneQuestion.asker_name}, Date Asked: {oneQuestion.question_date.slice(0, 10)}   |   Helpful? <u class="yes-underline">Yes</u> ({oneQuestion.question_helpfulness})   |   <u class="report-underline"> Add Answer </u> </span>
+          </div>
           <div>A: Generic Answer</div>
-          <div>by {oneQuestion.asker_name}, {oneQuestion.question_date.slice(0, 10)}   |   Helpful? <u class="yes-underline">Yes</u> ({oneQuestion.question_helpfulness})   |   <u class="report-underline">Report</u></div>
+          <div>by</div>
         </div>
       ))}
     </div>
