41d51713df6c6f5dfd9007677a73083a292ed546
zachfry <iitsfry@gmail.com>
2021-12-28 19:49:02 -0600

    Carousel updated

--- client/src/overview/carousel/carousel.js
+++ client/src/overview/carousel/carousel.js
@@ -5,26 +5,23 @@ import Slides from './slides.js';
 import Thumbnails from './thumbnails.js';
 
 function Carousel() {
-  const {products, setProducts, cart, setCart, currentProductId, setCurrentProductId, currentTheme, setCurrentTheme, features, setFeatures, styles, setStyles} = useContext(MainContext);
-
-  useEffect(() => {
-
-  }, []);
+  const {products, setProducts, cart, setCart, currentProductId, setCurrentProductId, currentTheme, setCurrentTheme, features, setFeatures, styles, setStyles, currStyle, setCurrStyle, mainPicture, setMainPicture, mainPictures, setMainPictures} = useContext(MainContext);
 
   if (styles == null) {
     return (<div>LADEN...</div>)
   }
 
+
   return (
 <div className="slideshow-container">
   <Slides />
-  {/* <Thumbnails /> */}
-  <a className="prev" >&#10094;</a>
-  <a className="next" >&#10095;</a>
+  <Thumbnails />
 </div>
   );
 }
 
+
 //include plusSlides function onClick
 
 export default Carousel;
+
--- client/src/overview/carousel/thumbnails.js
+++ client/src/overview/carousel/thumbnails.js
@@ -2,13 +2,39 @@ import React, { useState, useContext, useEffect} from 'react';
 import {MainContext} from '../../contexts/contexts.js'
 import Axios from 'axios';
 import Thumbnail from './thumbnail.js'
+import Arrows from './arrows.js';
 
 function Thumbnails() {
-  const {products, setProducts, cart, setCart} = useContext(MainContext);
+  const {products, setProducts, cart, setCart, currentProductId, setCurrentProductId, currentTheme, setCurrentTheme, features, setFeatures, styles, setStyles, currStyle, setCurrStyle, mainPicture, setMainPicture, mainPictures, setMainPictures} = useContext(MainContext);
+
+  if (currStyle == null) {
+    return <div>Loading...</div>
+  }
+  let photos;
+  let currProdStyles;
+  var mainPhotosArr = ['d'];
+  //Find currentProduct in styles
+  styles.forEach(p => {
+    if (Number(p.product_id) === currentProductId) {
+      currProdStyles = p;
+    }
+  });
+
+  let stylesData = currProdStyles.results.map(style => {
+    if (style.style_id === currStyle) {
+      photos = style.photos;
+    }
+  });
+
+  let thumbnailsArr = photos.map(photo => {
+    mainPhotosArr.push(photo['url']);
+    return <Thumbnail url={photo['thumbnail_url']} photo={photo}/>
+  });
 
   return (
     <div className="dot-container">
-      <Thumbnail />
+      {thumbnailsArr}
+      <Arrows mainPhotosArr = {mainPhotosArr}/>
     </div>
   );
 }
