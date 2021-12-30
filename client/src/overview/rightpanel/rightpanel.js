4728d77385815ce30ffcdb019640e250daed1271
zachfry <iitsfry@gmail.com>
2021-12-29 19:42:29 -0600

    Dyamically rendered product info to screen. Added graphics, fonts, etc.

--- client/src/overview/rightpanel/rightpanel.js
+++ client/src/overview/rightpanel/rightpanel.js
@@ -1,21 +1,33 @@
 import React, { useState, useContext, useEffect} from 'react';
 import {MainContext} from '../../contexts/contexts.js'
 import Axios from 'axios';
-import ProductInfo from './productinfo.js';
 import StyleSelector from './styleselector.js';
-import SelectSize from './selectsize.js';
-import AddToCart from './addtocart.js';
 
 function RightPanel() {
-  const {products, setProducts, cart, setCart, currentProductId, setCurrentProductId, currentTheme, setCurrentTheme, features, setFeatures, styles, setStyles, currStyle, setCurrStyle, mainPicture, setMainPicture} = useContext(MainContext);
+  const {products, setProducts, cart, setCart, currentProductId, setCurrentProductId, currentTheme, setCurrentTheme, productInformation, setProductInformation, styles, setStyles, currStyle, setCurrStyle, mainPicture, setMainPicture} = useContext(MainContext);
+  if (productInformation == null) {
+    return <div>Loading...</div>
+  }
+  let {category, name, default_price} = productInformation[0];
 
   return (
       <div className="rightpanel" >
-        <ProductInfo />
-        <div>Style > Selected style</div>
+        <div style={{marginBottom: '8px', fontFamiliy: 'sans-serif', color: 'RGB(82,82,82)', fontSize: '13px', display: 'inline-flex', alignItems: 'baseline'}}>★ ★ ★ ★ ☆ <div style={{marginLeft: '5px', fontFamiliy: 'sans-serif', color: 'RGB(82,82,82)', fontSize: '11px', textDecoration: 'underline'}}>Read all reviews</div></div>
+        <div style={{marginBottom: '-1px', fontFamiliy: 'sans-serif', fontSize: '13px', letterSpacing: '0.5px', color: 'RGB(82,82,82)'}}>{category.toUpperCase()}</div>
+        <div style={{marginBottom: '15px', fontFamiliy: 'sans-serif', fontSize: '28px', fontWeight: 'bold', color: 'RGB(82,82,82)', letterSpacing: '0.5px'}}>{name}</div>
+        <div style={{marginBottom: '20px', fontSize: '14px', color: 'RGB(82,82,82)'}}>${default_price.split('.')[0]}</div>
+        <div style={{marginBottom: '8px', fontFamiliy: 'sans-serif', letterSpacing: '0.5px', color: 'RGB(82,82,82)', fontSize: '14px'}}><b>STYLE</b> > SELECTED STYLE</div>
         <StyleSelector />
-        <SelectSize />
-        <AddToCart />
+        <select className="selectsizetext">
+            <option value="test">SELECT SIZE</option>
+          </select>
+          <select className="quantitycarttext">
+            <option value="test">1</option>
+          </select>
+        <div style={{display: 'flex', flexDirection: 'row', marginTop: '20px'}}>
+          <button type="button" className="addbagbutton">ADD TO BAG</button>
+          <button type="button" className="starbutton">☆</button>
+        </div>
       </div>
   );
 }
