6493674abcfc9b67e91badf6fe8f4f4dd9053e17
Prith <prith98@gmail.com>
2021-12-30 17:24:49 -0800

    Created form, able to type text in form

--- client/src/qna/AddQuestion.js
+++ client/src/qna/AddQuestion.js
@@ -2,6 +2,8 @@ import React, { useState, useContext, useEffect, useRef } from 'react';
 import ReactDOM from 'react-dom';
 import {MainContext} from '../contexts/contexts.js'
 import axios from 'axios';
+import { Form } from './Form.js';
+
 
 function AddQuestion () {
 
@@ -19,11 +21,37 @@ function AddQuestion () {
     }
   }
 
+  const onSubmit = (event) => {
+    event.preventDefault(event);
+    console.log(event.target.name.value);
+    console.log(event.target.email.value);
+  }
+
   return ReactDOM.createPortal(
     <div className="container" ref={modalRef} onClick={closeModal}>
       <div className="modal">
         <h2>This is a Modal</h2>
-        <button onClick={() => setShowModal(false)}>Add A Question</button>
+        <form onSubmit={onSubmit}>
+          <div className="form-group">
+            <label htmlFor="name">Name</label>
+            <input className="form-control" id="name" />
+          </div>
+          <div className="form-group">
+            <label htmlFor="email">Email address</label>
+            <input
+              type="email"
+              className="form-control"
+              id="email"
+              placeholder="name@example.com"
+            />
+          </div>
+          <div className="form-group">
+            <button className="form-control btn btn-primary" type="submit">
+              Submit
+            </button>
+          </div>
+        </form>
+        <button onClick={() => setShowModal(false)}>Submit Question</button>
       </div>
     </div>,
     document.getElementById("app")
