a0e554fa63f1a53ee5d3379c1c65064bb6d9f4f6
zachfry <iitsfry@gmail.com>
2022-01-06 16:48:01 -0600

    Overview complete

--- client/src/overview/carousel/slide.js
+++ client/src/overview/carousel/slide.js
@@ -29,32 +29,31 @@ function Slide() {
 
   function zoom(e) {
     let original = e.target,
-        magnified = e.target,
-        style = original.style,
-        x = e.pageX - e.target.offsetLeft,
-        y = e.pageY - e.target.offsetTop,
-        imgWidth = original.offsetWidth ,
-        imgHeight = original.offsetHeight,
-        xperc = ((x/imgWidth) * 100),
-        yperc = ((y/imgHeight) * 100);
-    // //lets user scroll past right edge of image
-    // if(x > (.01 * imgWidth)) {
-    //   xperc += (.15 * xperc);
-    // }
+    magnified = e.target,
+    style = original.style,
+    x = e.pageX - e.target.offsetLeft,
+    y = e.pageY - e.target.offsetTop,
+    imgWidth = original.offsetWidth ,
+    imgHeight = original.offsetHeight,
+    xperc = ((x/imgWidth) * 100),
+    yperc = ((y/imgHeight) * 100);
 
-    // //lets user scroll past bottom edge of image
-    // if(y >= (.01 * imgHeight)) {
-    //   yperc += (.15 * yperc);
-    // };
-    let xlimit;
-    let ylimit;
-    // 668 × 1002 px width: 60:33 ||
-    if (xperc-30 < 100 && xperc-30 > 0) {
+    let xLimitLeft = 60;
+    let yLimitRight = 33;
+
+    var img = new Image();
+    img.src = mainPicture;
+
+    if (img.width > 668) {
+      xLimitLeft = 100;
+      yLimitRight = 0;
+    }
+    if (xperc-30 < xLimitLeft && xperc-30 > yLimitRight) {
       style.backgroundPositionX = (xperc - 30) + '%';
     }
 
 
-    if (yperc-9 < 100 && yperc-9 > 0) {
+    if (yperc-30 < 100 && yperc-30 > 0) {
       style.backgroundPositionY = (yperc - 30) + '%';
     }
 
@@ -64,7 +63,7 @@ function Slide() {
 
   let zoomDiv = zoomEnabled === true ? <div style={{backgroundImage: 'URL(' + mainPicture + ')', backgroundRepeat: 'no-repeat', transform:'scale(2.5)', backgroundPositionX: '50%', cursor: extend === true ? 'crosshair':'zoom-in', height: '100%', width: '100%'}} onMouseMove={zoom} onClick={extendView}> </div>:<img src={mainPicture} style={{cursor: extend === true ? 'crosshair':'zoom-in', height: '100%', width: extend===true ? '160%':'100%', objectFit: 'cover', webkitTransition: 'width 1s ease, height 1s ease'}} onClick={extendView}/>
   return (
-    <div className="mySlides fade" style={{width: zoomEnabled===true ? '1205px':'94%', backgroundColor:'yellow', height:'590px', display: 'block', overflow: zoomEnabled === true ? 'hidden':''}}>
+    <div className="mySlides fade" style={{width: zoomEnabled===true ? '1205px':'94%', backgroundColor:'white', height:'590px', display: 'block', overflow: zoomEnabled === true ? 'hidden':''}}>
       <FaExpand className="extend" style={{display: zoomEnabled === true ? 'none':'', color: 'black', right: extend===true ? '-371px':'77px', webkitTransition: 'right 1s ease'}} onClick={extendToggle}/>
       {zoomDiv}
     </div>
