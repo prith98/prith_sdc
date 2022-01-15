832f2697df5a23c4fd2a6792c828ddd63d0f80f2
Prith <prith98@gmail.com>
2022-01-14 21:03:27 -0800

    some work

--- database/index.js
+++ database/index.js
@@ -11,14 +11,6 @@ const pool = new Pool({
   port: 5432,
 });
 
-const client = new Client({
-  user: "prithjaganathan",
-  host: "localhost",
-  database: "qadb",
-  password: "password",
-  port: 5432,
-});
-
 const getQuestions = (request, response) => {
   let { product_id, page, count } = request.query;
   page = page || 1;
@@ -39,15 +31,35 @@ const getQuestions = (request, response) => {
         'reported', q.reported
       )
     )
-  ) FROM questions q WHERE product_id = ${product_id} LIMIT ${count}`;
+  ) FROM questions q WHERE q.product_id = ${product_id} LIMIT ${count};`;
+
+  // const queryString = `SELECT (json_agg(
+  //     json_build_object(
+  //     'question_id', q.question_id,
+  //     'question_body', q.question_body,
+  //     'question_date', q.question_date,
+  //     'asker_name', q.asker_name,
+  //     'question_helpfulness', q.question_helpfulness,
+  //     'reported', q.reported,
+  //     'answers', json_build_object(
+  //         'id', a.answers_id,
+  //         'body', a.body,
+  //         'date', a.date,
+  //         'answerer_name', a.answerer_name,
+  //         'helpfulness', a.helpfulness
+  //     )
+  //   )
+  // )
+  // ) FROM questions q INNER JOIN answers a on a.id_questions = q.question_id GROUP BY q.question_id LIMIT ${count};`
 
   pool.query(queryString, (err, results) => {
     if (err) {
       console.log(err);
       response.send(err);
     }
-    resultsObject.results = results.rows[0]["json_agg"];
-    response.status(200).send(resultsObject);
+    // resultsObject.results = results.rows[0]["json_agg"];
+    console.log(results.rows[0]["json_agg"]);
+    response.status(200).send(results.rows[0]["json_agg"]);
   });
 };
 
@@ -56,18 +68,34 @@ const getAnswers = (request, response) => {
   let { page, count } = request.query;
   page = page || 1;
   count = count || 5;
-  pool.query(
-    "SELECT (answers_id, body, date, answerer_name, helpfulness) FROM answers WHERE id_questions = ($1) LIMIT ($2)",
-    [question_id, count],
-    (err, results) => {
-      if (err) {
-        console.log(err);
-        response.send(err);
-      }
-      console.log(results.rows);
-      response.status(200).json(results.rows[0]["json_agg"]);
+
+  let responseObject = {
+    question: question_id,
+    page: page,
+    count: count,
+    results: "",
+  };
+
+  const queryString = `SELECT (json_agg(
+    json_build_object(
+      'answer_id', a.answers_id,
+      'body', a.body,
+      'date', a.date,
+      'answerer_name', a.answerer_name,
+      'helpfulness', a.helpfulness
+      )
+    )
+  ) FROM answers a WHERE a.id_questions = ${question_id} LIMIT ${count};`;
+
+  pool.query(queryString, (err, results) => {
+    if (err) {
+      console.log(err);
+      response.send(err);
     }
-  );
+    console.log(results.rows);
+    responseObject.results = results.rows[0]["json_agg"];
+    response.status(200).json(responseObject);
+  });
 };
 
 pool.connect((err) => {
