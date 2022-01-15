8754c60d23c4d9ee445b3559fb2d7e792209045f
Prith <prith98@gmail.com>
2022-01-15 14:54:47 -0800

    Added k6 test suite, created test(s)

---
+++ database/tests/script.js
@@ -0,0 +1,11 @@
+import http from 'k6/http';
+import { sleep } from 'k6';
+export const options = {
+  vus: 1000,
+  duration: '10s',
+};
+export default function () {
+  const randomProductId = (Math.random() * 1000011).toFixed(0);
+  http.get(`http://localhost:3000/qa/questions?product_id=${randomProductId}`);
+  sleep(1);
+}
